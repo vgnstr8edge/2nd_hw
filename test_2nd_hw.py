f5cfd5d4a5f54be96d3639a96d4a79a58bdd417b
Dmitrii Nasedkin <2_tone@mail.ru>
2023-02-21 22:58:22 +0300

    обновил негативный тест, перенес данные из тестов в фикстуру, добавил явно версию селена

--- test_2nd_hw.py
+++ test_2nd_hw.py
@@ -5,22 +5,20 @@ import pytest
 
 @pytest.fixture
 def browser_settings():
-    browser.config.window_height = 900
-    browser.config.window_width = 750
+    browser.config.browser_name = 'firefox'
+    browser.config.hold_browser_open = True
+    browser.open('https://google.com')
+    browser.config.window_height = 1000
+    browser.config.window_width = 1500
     return browser
 
 
 def test_positive(browser_settings):
-    browser.config.browser_name = 'firefox'
-    browser.config.hold_browser_open = True
-    browser.open('https://google.com')
     browser.element('.RNNXgb input[name=q]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_negative(browser_settings):
-    browser.config.browser_name = 'firefox'
-    browser.config.hold_browser_open = True
-    browser.open('https://google.com')
-    browser.element('.RNNXgb input[name=q]').should(be.blank).type('yashaka/c##/fkdlkflkjfj').press_enter()
+    browser.element('.RNNXgb input[name=q]').should(be.blank).type('зщкзхух').press_enter()
     browser.element('[id=center_col]').should(have.text('ничего не найдено'))
+
